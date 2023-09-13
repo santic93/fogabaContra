ba2d2f3225dd0a290493ecceaf2bdb142078816d
Santiago Castro <scastro@fogaba.com>
2023-09-13 17:10:48 -0300

    avances 19/9

--- front/src/Components/Espere/Espere.jsx
+++ front/src/Components/Espere/Espere.jsx
@@ -1,7 +1,7 @@
 import './Espere.css';
 export default function Espere() {
   return (
-    <div className='sssss spinner-grow text-primary' role='status'>
+    <div className='sssss spinner-grow text-danger' role='status'>
       <span className='visually-hidden'>Loading...</span>
     </div>
   );
