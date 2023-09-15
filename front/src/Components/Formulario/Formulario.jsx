8617fa2ae7d6e6aa4ef2a4ab41cb60b6396cf4f9
Santiago Castro <scastro@fogaba.com>
2023-09-15 13:12:54 -0300

    + avances

--- front/src/Components/Formulario/Formulario.jsx
+++ front/src/Components/Formulario/Formulario.jsx
@@ -281,7 +281,7 @@ export default function Formulario() {
           <hr class='border border-primary border-3 opacity-75'></hr>
           <div className='mb-3'>
             <h1>
-              <button type="button" className='btn btn-primary btn-lg'>
+              <button type="button" className='btn btn-primary btn-lg consultaArbaBTN'>
                 <a
                   href='https://consultas.arba.gov.ar/ConsultasGenerales/inicioEstadoDeudaCategoria.do'
                   target='_blank'
